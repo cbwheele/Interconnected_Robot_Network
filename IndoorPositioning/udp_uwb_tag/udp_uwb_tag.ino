d59e0af3c14859959b897d8b7c7a0c2a169175ff
Caleb Wheeler <cbwheele@ncsu.edu>
2022-09-01 14:34:20 -0400

    Initial commit with code from other repo and UDP connection

---
+++ IndoorPositioning/udp_uwb_tag/udp_uwb_tag.ino
@@ -0,0 +1,146 @@
+#include <SPI.h>
+#include <DW1000Ranging.h>
+#include <WiFi.h>
+#include "link.h"
+
+#define SPI_SCK 18
+#define SPI_MISO 19
+#define SPI_MOSI 23
+#define DW_CS 4
+#define PIN_RST 27
+#define PIN_IRQ 34
+
+const char *ssid = "ncsu";
+const char *password = "";
+const char *host = "172.20.10.2";
+WiFiClient client;
+
+struct MyLink *uwb_data;
+int index_num = 0;
+long runtime = 0;
+String all_json = "";
+
+WiFiUDP udp;
+unsigned int localPort = 2983;
+char packetBuffer[256]; //buffer to hold incoming packet
+
+void setup()
+{
+    Serial.begin(115200);
+
+    Serial.println("WiFi Mac Address:");
+    Serial.println(WiFi.macAddress());
+
+    WiFi.mode(WIFI_STA);
+    WiFi.setSleep(false);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("Connected");
+    Serial.print("IP Address:");
+    Serial.println(WiFi.localIP());
+
+    Serial.println("About to create UDP port to listen to");
+    udp.begin(localPort);
+
+    if (client.connect(host, 80))
+    {
+        Serial.println("Success");
+        client.print(String("GET /") + " HTTP/1.1\r\n" +
+                     "Host: " + host + "\r\n" +
+                     "Connection: close\r\n" +
+                     "\r\n");
+    }
+
+    delay(1000);
+
+    //init the configuration
+    SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
+    DW1000Ranging.initCommunication(PIN_RST, DW_CS, PIN_IRQ);
+    DW1000Ranging.attachNewRange(newRange);
+    DW1000Ranging.attachNewDevice(newDevice);
+    DW1000Ranging.attachInactiveDevice(inactiveDevice);
+
+    //we start the module as a tag
+    DW1000Ranging.startAsTag("7D:00:22:EA:82:60:3B:9C", DW1000.MODE_LONGDATA_RANGE_LOWPOWER);
+
+    uwb_data = init_link();
+    
+}
+
+void loop()
+{
+
+
+  int packetSize = udp.parsePacket();
+  if (packetSize) {
+    Serial.print("Received packet of size ");
+    Serial.println(packetSize);
+    Serial.print("From ");
+    IPAddress remoteIp = udp.remoteIP();
+    Serial.print(remoteIp);
+    Serial.print(", port ");
+    Serial.println(udp.remotePort());
+    // read the packet into packetBufffer
+    int len = udp.read(packetBuffer, 255);
+    if (len > 0) {
+      packetBuffer[len] = 0;
+    }
+    Serial.println("Contents:");
+    Serial.println(packetBuffer);
+    udp.endPacket();
+  }
+
+
+    DW1000Ranging.loop();
+    if ((millis() - runtime) > 1000)
+    {
+        make_link_json(uwb_data, &all_json);
+        send_udp(&all_json);
+        runtime = millis();
+    }
+}
+
+void newRange()
+{
+    char c[30];
+
+    Serial.print("from: ");
+    Serial.print(DW1000Ranging.getDistantDevice()->getShortAddress(), HEX);
+    Serial.print("\t Range: ");
+    Serial.print(DW1000Ranging.getDistantDevice()->getRange());
+    Serial.print(" m");
+    Serial.print("\t RX power: ");
+    Serial.print(DW1000Ranging.getDistantDevice()->getRXPower());
+    Serial.println(" dBm");
+    fresh_link(uwb_data, DW1000Ranging.getDistantDevice()->getShortAddress(), DW1000Ranging.getDistantDevice()->getRange(), DW1000Ranging.getDistantDevice()->getRXPower());
+}
+
+void newDevice(DW1000Device *device)
+{
+    Serial.print("ranging init; 1 device added ! -> ");
+    Serial.print(" short:");
+    Serial.println(device->getShortAddress(), HEX);
+
+    add_link(uwb_data, device->getShortAddress());
+}
+
+void inactiveDevice(DW1000Device *device)
+{
+    Serial.print("delete inactive device: ");
+    Serial.println(device->getShortAddress(), HEX);
+
+    delete_link(uwb_data, device->getShortAddress());
+}
+
+void send_udp(String *msg_json)
+{
+    if (client.connected())
+    {
+        client.print(*msg_json);
+        Serial.println("UDP send");
+    }
+}
